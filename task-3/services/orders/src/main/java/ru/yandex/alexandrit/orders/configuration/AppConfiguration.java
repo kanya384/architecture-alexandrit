f640bf75948eeeb13bf0325ac05c29316880f021
laurkan <laurkan@Mac-mini-laurkan.local>
2026-06-24 23:13:52 +0300

    task-3 fix

--- task-3/services/orders/src/main/java/ru/yandex/alexandrit/orders/configuration/AppConfiguration.java
+++ task-3/services/orders/src/main/java/ru/yandex/alexandrit/orders/configuration/AppConfiguration.java
@@ -3,12 +3,19 @@ package ru.yandex.alexandrit.orders.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfiguration {
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder()
+    public RestClient restClient(RestClient.Builder builder) {
+
+        return builder
                 .build();
     }
 }
